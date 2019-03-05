ff45dfe2fd76393ebf61283c8ada6d20b70a30f4
Paul Derbyshire <pderbs@icloud.com>
2019-03-05 17:35:20 +0000

    No need for IBOutlets

--- StarApp/ViewController.swift
+++ StarApp/ViewController.swift
@@ -9,13 +9,6 @@ import UIKit
 
 class ViewController: UIViewController {
 	
-	@IBOutlet var button1: UIButton!
-	@IBOutlet var button2: UIButton!
-	@IBOutlet var button3: UIButton!
-	@IBOutlet var button4: UIButton!
-	@IBOutlet var button5: UIButton!
-	
-	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 	}
